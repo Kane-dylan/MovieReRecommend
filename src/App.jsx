afccb6526297b8c131cfcf6a5973550cb47c4fa0
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-08 12:28:07 +0530

    movie app have been created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 import Loader from "./components/Loader";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
-import { getTrendingMovies,updateSearchCount } from "./AppWrite";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
